10424a07ec978ce051d03c6d782dc4cd35adabd3
YH Lin <yh_lin@sigmadesigns.com>
2012-07-06 17:42:37 -0700

    A better fix for USB init.

--- drivers/usb/core/tangox-usb.c
+++ drivers/usb/core/tangox-usb.c
@@ -145,21 +145,11 @@ void tangox_usb_init(int ctrl)
 		return;
 	}
 
-#if defined(CONFIG_TANGO3)
+#if defined(CONFIG_TANGO3) || defined(CONFIG_TANGO4)
 	/* Reset USB host(s), redo USB initialization */
-	gbus_write_reg32(TANGOX_CTRL0_BASE, gbus_read_reg32(TANGOX_CTRL0_BASE) | 0x2);
-	if (((chip_id & 0xfff0) == 0x8670) || ((chip_id & 0xfff0) == 0x8680))
-		gbus_write_reg32(TANGOX_CTRL1_BASE, gbus_read_reg32(TANGOX_CTRL1_BASE) | 0x2);
+	gbus_write_reg32(tangox_ctrl_base[ctrl] + 0x0, gbus_read_reg32(tangox_ctrl_base[ctrl] + 0x0) | 0x2);
 	mdelay(2);
-	gbus_write_reg32(TANGOX_CTRL0_BASE, gbus_read_reg32(TANGOX_CTRL0_BASE) & ~0x2);
-	if (((chip_id & 0xfff0) == 0x8670) || ((chip_id & 0xfff0) == 0x8680))
-		gbus_write_reg32(TANGOX_CTRL1_BASE, gbus_read_reg32(TANGOX_CTRL1_BASE) & ~0x2);
-	mdelay(2);
-#elif defined(CONFIG_TANGO4)
-	/* Reset USB host(s), redo USB initialization */
-	gbus_write_reg32(TANGOX_CTRL0_BASE, gbus_read_reg32(TANGOX_CTRL0_BASE) | 0x2);
-	mdelay(2);
-	gbus_write_reg32(TANGOX_CTRL0_BASE, gbus_read_reg32(TANGOX_CTRL0_BASE) & ~0x2);
+	gbus_write_reg32(tangox_ctrl_base[ctrl] + 0x0, gbus_read_reg32(tangox_ctrl_base[ctrl] + 0x0) & ~0x2);
 	mdelay(2);
 #endif
 
