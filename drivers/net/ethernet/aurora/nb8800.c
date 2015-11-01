31bac07044a613c02a416fb686c39a100452c178
Mans Rullgard <mans@mansr.com>
2015-11-01 14:00:45 +0000

    ethernet: nb8800: add MODULE_AUTHOR

--- drivers/net/ethernet/aurora/nb8800.c
+++ drivers/net/ethernet/aurora/nb8800.c
@@ -1129,4 +1129,5 @@ static struct platform_driver nb8800_driver = {
 module_platform_driver(nb8800_driver);
 
 MODULE_DESCRIPTION("Aurora AU-NB8800 Ethernet driver");
+MODULE_AUTHOR("Mans Rullgard <mans@mansr.com>");
 MODULE_LICENSE("GPL");
